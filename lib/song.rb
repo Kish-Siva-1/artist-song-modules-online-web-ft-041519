820a8f98f0a763ca98afabaa8d4132d7f3270603
Kish Siva <rodes88@msn.com>
2019-05-04 13:13:42 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,9 +2,11 @@ require 'pry'
 
 class Song
   
-  extend Memorable 
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods 
+  
   extend Findable
-  include Paramable
+  extend Paramable
   
   attr_accessor :name
   attr_reader :artist
